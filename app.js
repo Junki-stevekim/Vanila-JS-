538c5f9a54d84428739962fa4bee25b552128387
junki-stevekim <junkistevekim@gmail.com>
2022-09-27 21:24:23 +0900

    First day

---
+++ app.js
@@ -0,0 +1,16 @@
+//  계산기 만들어보기
+
+// 유저의나이를 물어보기
+
+const age = parseInt(prompt("How Old Are YOU?"));
+
+
+console.log(isNaN(age));
+
+if(isNaN(age) ==true){
+    console.log("숫자를입력해주세요")
+}else{
+    
+}   
+
+isNaN();
