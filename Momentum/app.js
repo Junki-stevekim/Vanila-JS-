2cd9f5a5392eac5a2f8a8b88b3b7c827a905cc34
junki-stevekim <junkistevekim@gmail.com>
2022-09-28 20:56:25 +0900

    Directory

---
+++ Momentum/app.js
@@ -0,0 +1,9 @@
+// JS를 이용해 조금더 HTML를 이해해보자
+// document-> 가장핵심적인 JS의 객체
+// JavaScript 는 HTML을 불러와 읽어들인다. 
+
+
+document.title; // HTML의 title을 읽어오기 결과:Momentum
+document.title="Hi"; // title을 읽어와 값을 변경하기.
+
+document.getElementById("title"); // HTML의 요소를 불러오는 아주아주아주아주 중요한 Object
