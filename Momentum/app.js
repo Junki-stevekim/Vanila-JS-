c02cc96e582685cf0751a3e33bec6872dd28d1a4
junki-stevekim <junkistevekim@gmail.com>
2022-10-16 14:54:51 +0900

    login form

--- Momentum/app.js
+++ Momentum/app.js
@@ -1,17 +1,30 @@
 // login button,TO-DO list
 
+const loginForm=document.querySelector("#login-form");
 const loginInput=document.querySelector("#login-form input");
-const loginButton=document.querySelector("#login-form button");
+const greeting=document.querySelector("#greeting");
 
-function onLoginBtnClick(){
-const username = loginInput.value;
-if(username===""){
-     alert("Please write your name");
+const HIDDEN_CLASS="hidden";
+const USERNAME_KEY="username";
 
-}else if(username.length>15){
-     alert("Your name is too long")
+function onLoginSubmit(event){
+event.preventDefault();
+loginForm.classList.add(HIDDEN_CLASS);
+const username=loginInput.value;
+localStorage.setItem(USERNAME_KEY,username);
+greeting.innerText=`Hello ${username}`;
+greeting.classList.remove(HIDDEN_CLASS);
+
 }
+
+
+
+const savedUsername=localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASS);
+    loginForm.addEventListener("submit",onLoginSubmit);
+}else{
+    greeting.innerText=`Hello ${savedUsername}`;
+    greeting.classList.remove(HIDDEN_CLASS);
 }
-
-
-loginButton.addEventListener("click",onLoginBtnClick);
