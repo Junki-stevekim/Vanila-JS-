5f9af215294dc516c6856c0c1e890aea7463284c
junki-stevekim <junkistevekim@gmail.com>
2022-10-23 22:34:56 +0900

    quotes,img

---
+++ Momentum/js/background.js
@@ -0,0 +1,13 @@
+const images = [
+    "0.jpeg",
+    "1.jpeg",
+    "2.jpeg"
+];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage); // html로 생성된 태그를 보내준다.
