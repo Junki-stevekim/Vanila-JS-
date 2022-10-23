7fd97d5ad0db52c4b3c05eec5477f1c9b3043fd1
junki-stevekim <junkistevekim@gmail.com>
2022-10-23 22:05:15 +0900

    Clock change

--- Momentum/js/clock.js
+++ Momentum/js/clock.js
@@ -1,9 +1,13 @@
-const clock = document.querySelector("clock");
+const clock = document.querySelector("h2#clock");
 
-function sayHello(){
-    console.log("hello");
+function getClock(){
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+    clock.innerText=`${hours}:${minutes}:${seconds}`;
 }
 
+getClock();
+setInterval(getClock,1000); // 5초마다 호출
 
-setTimeout(sayHello,5000);
- 5초마다 호출
