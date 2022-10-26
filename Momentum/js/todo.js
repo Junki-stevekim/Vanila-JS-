74093522763c643eab512b67260309adefdef715
junki-stevekim <junkistevekim@gmail.com>
2022-10-26 22:57:58 +0900

    TO-DO ADD

--- Momentum/js/todo.js
+++ Momentum/js/todo.js
@@ -1,12 +1,32 @@
-const toDoForm=document.getElementById("todo-form");
-const toDoInput=document.querySelector("#todo-form input");
-const toDoList=document.getElementById("todo-list");
 
-function handleToDoSubmit(event){
-    event.preventDefault();
-    const newTodo=toDoInput.value; // input 값을 새로운 변수에 담아준다.
-    toDoInput.value="";
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = document.querySelector("#todo-form input");
+const toDoList = document.getElementById("todo-list");
+
+
+
+function deleteTodo(){
 
 }
 
-toDoFrom.addEventListener("submit",handleToDoSubmit);
+
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText="★";
+  button.addEventListener("click" ,deleteTodo);
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  paintToDo(newTodo);
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
