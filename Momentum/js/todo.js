3664c3d9a9726de8e7658b18dec42eba0832c660
junki-stevekim <junkistevekim@gmail.com>
2022-10-27 22:18:08 +0900

    save to do

--- Momentum/js/todo.js
+++ Momentum/js/todo.js
@@ -3,10 +3,18 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function savingTodo(){
+  localStorage.setItem("todos", JSON.stringify(toDos)); // text 값을 String으로 변환
+
+}
+
 
 
 function deleteTodo(event){
-  console.log(event.target.parentElement); // 어떤 버튼을 삭제했는지 알수있다.
+  const li = event.target.parentElement;
+  li.remove(); // 어떤 버튼을 삭제했는지 알수있다.
 
 }
 
@@ -27,7 +35,9 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
+  toDos.push(newTodo); // 배열로 전송
   paintToDo(newTodo);
+  savingTodo(); // newTodo를 local storage 에 저장
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
